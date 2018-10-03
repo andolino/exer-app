0ba8008a0045dbb16b51d1e1a21df4e4028b4998
andolino <dondonpentavia@gmail.com>
2018-10-03 13:59:24 +0800

    initial commit

---
+++ src/components/Transition.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
+import '../css/App.css';
+
+class TransitionComp extends Component{
+
+    render(){
+        return(
+            <div>
+                Transition 
+            </div>
+        )
+    }
+}
+
+
+export default TransitionComp;
