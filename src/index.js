d0a70610775aed056f5556a7f276ff9846641a08
andolino <dondonpentavia@gmail.com>
2018-10-03 23:19:27 +0800

    this is my second commit to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Transition from 'react-transition-group/Transition';
+
+
 class App extends Component {
+
+  state = {
+    show: false
+  }
+
+  showDiv = () => {
+    this.setState({
+      show: !this.state.show ? true : false
+    });  
+  }
+
   render(){
     return(
-      <div>Hello World</div>
+      <div>
+        <Transition
+          in={this.state.show}
+          timeout={2000}
+          mountOnEnter
+          unmountOnExit
+        >
+        {(state) => {
+          return(
+            <div style={{
+              background: 'red',
+              height: '100px',
+              transition: 'all 2s ease',
+              opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+            }}>
+            <p>{state}</p>
+            </div>
+          );
+        }}
+        </Transition>        
+        <div className=""
+          onClick={this.showDiv}
+        >
+          Click me
+        </div>
+      </div>
     );
   }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
-
